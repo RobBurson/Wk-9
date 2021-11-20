041e4791166d0aa68879dff473685cd33d50b3ba
Rob <robburson07@gmail.com>
2021-11-19 20:23:07 -0600

    updated gitignore index js and generateMarkdown js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,62 @@
-// TODO: Include packages needed for this application
+const inq = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//    <!-- Questions -->
+const questions = [
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    {
+        type:'input',
+        name:'github',
+        message:'What is your github username?',
+    },
+    {
+        type:'input',
+        name:'email',
+        message:'What is your email address?',
+    },
+    {
+        type:'input',
+        name:'title',
+        message:'What is your Project Title?',
+    },
+    {
+        type:'input',
+        name:'details',
+        message:'What are your Project Detailsf?',
+        
+    },
+    {
+        type:'input',
+        name:'installation',
+        message:'What are the steps for installation?',
+    },
+    {
+        type:'input',
+        name:'benefits',
+        message:'What are some benefits for the use of your project?',
+    },
+    {
+        type:'list',
+        name:'licenses',
+        message:'What type of license(s) was chosen for your project?',
+        choices:['MIT License', "APACHE 2.0", "None"]
+    },
+    {
+       type:'input',
+       name:'contributions',
+       message:'Were there any contributions and/or collaborators to your project',
+    },
+    {
+      type:'input',
+      name:'test',
+      message:'What steps were taken to test project?',
+   },
+]
+    function init() {
+      inq.prompt(questions).then((data) => {
+          console.log(data)
+      fs.writeFile('GeneratedREADME/GeneratedREADME.md', generateMarkdown(data),(err) => err? console.error(err) : console.log("Success!"))})
+   }
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
 init();
